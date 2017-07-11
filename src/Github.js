8e94082bcf25e6403b0521d475fdb57ea1545a96
Mark Gelfius <mgelfius@purdue.edu>
2017-07-11 15:26:32 -0400

    Add submit handler to form that pushes new history record

--- src/Github.js
+++ src/Github.js
@@ -10,13 +10,19 @@ class Github extends Component {
         this.setState({ username: ev.target.value})
     }
 
+    handleSubmit = (ev) =>{
+        ev.preventDefault()
+        this.props.history.push(`/github/${this.state.username}`)
+        this.setState({username: ''})
+    }
+
     render(){
         return(
             <div className="github">
                 <img className="github-logo" src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png"
                  alt="GitHub logo"
                  />
-                 <form>
+                 <form onSubmit={this.handleSubmit}>
                     <div>
                         <input type="text"
                         value={this.state.username}
